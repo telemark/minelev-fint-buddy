dde0fad37438fd772f99dd8972c4166539ad608c
Alexander Zeiher <alexander@zeiher.se>
2018-09-28 14:13:13 +0200

    fix sensetiv data

--- src/lib/fint-client.js
+++ src/lib/fint-client.js
@@ -1,10 +1,25 @@
 const getToken = require('./get-token')
 const getData = require('./get-data')
 const logger = require('./logger')
-const config = require('../config')
 
 module.exports = async () => {
-  const options = config.fint
+  const options = {
+    url: process.env.FINT_AUTH_URL,
+    credentials: {
+      client: {
+        client_id: process.env.FINT_CLIENT_ID,
+        client_secret: process.env.FINT_CLIENT_SECRET
+      },
+      auth: {
+        username: process.env.FINT_AUTH_USERNAME,
+        password: process.env.FINT_AUTH_PASSWORD,
+        grant_type: 'password',
+        scope: 'fint-client'
+      }
+    },
+    orgId: process.env.FINT_ORG_ID
+  }
+
 
   try {
     const { access_token: token, expires_in: expires } = await getToken(options)
