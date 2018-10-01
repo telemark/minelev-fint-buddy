391b4ee8ffc024abbdac29c9de86a53692f67cc5
Jonas Enge <jonas.enge@gmail.com>
2018-10-01 12:49:02 +0200

    Simplifies data-mapper

--- src/routes/data-mapper.js
+++ src/routes/data-mapper.js
@@ -1,5 +1,5 @@
-exports.StudentGroup = async (elev, person, skole, groups) => {
-  const data = [
+exports.StudentGroup = (elev, person, skole, groups) => (
+  [
     {
       firstName: person.navn.fornavn,
       middleName: person.navn.mellomnavn || null,
@@ -17,11 +17,10 @@ exports.StudentGroup = async (elev, person, skole, groups) => {
       groups: groups
     }
   ]
-  return data
-}
+)
 
-exports.Group = (skole, group) => {
-  const data = [
+exports.Group = (skole, group) => (
+  [
     {
       id: group.what, // what is this?
       description: group.what, // what is this?
@@ -31,11 +30,10 @@ exports.Group = (skole, group) => {
       contactTeacher: '--------------' // when is this true or false?
     }
   ]
-  return data
-}
+)
 
-exports.Student = (elev, person) => {
-  const data = {
+exports.Student = (elev, person) => (
+  {
     firstName: person.navn.fornavn,
     middleName: person.navn.mellomnavn || null,
     lastName: person.navn.etternavn,
@@ -45,11 +43,10 @@ exports.Student = (elev, person) => {
     mail: person.kontaktinformasjon.epostadresse || null,
     userName: null
   }
-  return data
-}
+)
 
-exports.Teacher = (personalressurs, person, skole) => {
-  const data = [
+exports.Teacher = (personalressurs, person, skole) => (
+  [
     {
       firstName: person.navn.fornavn,
       middleName: person.navn.mellomnavn || null,
@@ -64,61 +61,50 @@ exports.Teacher = (personalressurs, person, skole) => {
       organizationNumber: skole.organisasjonsnummer.identifikatorverdi
     }
   ]
-  return data
-}
+)
 
-exports.contactTeacher = (personalressurs, contactGroup) => {
-  const data =
-    {
-      username: personalressurs.brukernavn.identifikatorverdi,
-      groupId: contactGroup.systemId.identifikatorverdi, // what id
-      mail: personalressurs.kontaktinformasjon.epostadresse
-    }
+exports.contactTeacher = (personalressurs, contactGroup) => (
+  {
+    username: personalressurs.brukernavn.identifikatorverdi,
+    groupId: contactGroup.systemId.identifikatorverdi, // what id
+    mail: personalressurs.kontaktinformasjon.epostadresse
+  }
+)
 
-  return data
-}
-
-exports.contactClass = contactClass => {
-  const contactClassId =
-    {
-      id: contactClass.navn // or contactClass.systemId.identifikatorverdi
-    }
-
-  return contactClassId
-}
+exports.contactClass = contactClass => (
+  {
+    id: contactClass.navn // or contactClass.systemId.identifikatorverdi
+  }
+)
 
 exports.allGroupUrls = forhold => {
-  const Urls = []
+  const urls = []
   if (forhold._links.kontaktlarergruppe) {
     for (const kontaktlarergruppe of forhold._links.kontaktlarergruppe) {
-      Urls.push(kontaktlarergruppe.href)
+      urls.push(kontaktlarergruppe.href)
     }
   }
   if (forhold._links.basisgruppe) {
     for (const basisgruppe of forhold._links.basisgruppe) {
-      Urls.push(basisgruppe.href)
+      urls.push(basisgruppe.href)
     }
   }
   if (forhold._links.undervisningsgruppe) {
     for (const undervisningsgruppe of forhold._links.undervisningsgruppe) {
-      Urls.push(undervisningsgruppe.href)
+      urls.push(undervisningsgruppe.href)
     }
   }
-  return Urls
+  return urls
 }
 
-exports.personUrl = elev => {
-  return elev._links.person[0].href
-}
-exports.elevforholdUrl = elev => {
-  return elev._links.elevforhold[0].href
-}
-exports.elevUrl = elevforhold => {
-  return elevforhold._links.elev[0].href
-}
-exports.skoleUrl = elevforhold => {
-  return elevforhold._links.skole[0].href
-}
+exports.personUrl = elev => elev._links.person[0].href
+
+exports.elevforholdUrl = elev => elev._links.elevforhold[0].href
+
+exports.elevUrl = elevforhold => elevforhold._links.elev[0].href
+
+exports.skoleUrl = elevforhold => elevforhold._links.skole[0].href
+
 exports.memberUrls = group => {
   const memberUrls = []
   for (const member of group._links.elevforhold) {
@@ -126,15 +112,12 @@ exports.memberUrls = group => {
   }
   return memberUrls
 }
-exports.skoleressursUrl = personalressurs => {
-  return personalressurs._links.skoleressurs[0].href
-}
-exports.undervisningsforholdUrl = skoleressurs => {
-  return skoleressurs._links.undervisningsforhold[0].href
-}
-exports.personalressursUrl = skoleressurs => {
-  return skoleressurs._links.personalressurs[0].href
-}
+exports.skoleressursUrl = personalressurs => personalressurs._links.skoleressurs[0].href
+
+exports.undervisningsforholdUrl = skoleressurs => skoleressurs._links.undervisningsforhold[0].href
+
+exports.personalressursUrl = skoleressurs => skoleressurs._links.personalressurs[0].href
+
 exports.undervisningsforholdUrls = group => {
   const undervisningsforholdUrls = []
   for (const undervisningsforhold of group._links.undervisningsforhold) {
@@ -142,6 +125,7 @@ exports.undervisningsforholdUrls = group => {
   }
   return undervisningsforholdUrls
 }
+
 exports.contactGroupsUrls = forhold => {
   const contactGroupsUrls = []
   for (const group of forhold._links.kontaktlærergruppe) {
